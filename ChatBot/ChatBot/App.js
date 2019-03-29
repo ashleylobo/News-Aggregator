a143df4072357ede4cb78c6a35b4e0635a96eed7
Ashley Lobo <ashleyl@rediffmail.com>
2019-03-29 20:53:04 +0530

    ChatBot Done

--- ChatBot/ChatBot/App.js
+++ ChatBot/ChatBot/App.js
@@ -1,52 +1,71 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { GiftedChat } from "react-native-gifted-chat";
+import React, { Component } from 'react';  
+import { StyleSheet, Text, View } from 'react-native';  
+import { GiftedChat } from 'react-native-gifted-chat';  
+import { Dialogflow_V2 } from 'react-native-dialogflow';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    let firstMsg = {
-      _id: 1,
-      text: "Hello CoderSchool Fan!",
-      createdAt: new Date(),
-      user: {
-        _id: 2,
-        name: "Kunal Jathal",
-        avatar: "http://www.coderschool.vn/system/instructors/avatar_images/000/000/001/original/kunal.jpg?1531064314"
+import { dialogflowConfig } from './env';
+
+const BOT_USER = {  
+  _id: 2,
+  name: 'FAQ Bot',
+  avatar: 'https://i.imgur.com/7k12EPD.png'
+};
+
+class App extends Component {  
+  state = {
+    messages: [
+      {
+        _id: 1,
+        text: `Hi! I am the FAQ bot 🤖 from Jscrambler.\n\nHow may I help you with today?`,
+        createdAt: new Date(),
+        user: BOT_USER
       }
-    }
+    ]
+  };
 
-    this.state = {
-      messages: [firstMsg]
-    }
+  componentDidMount() {
+    Dialogflow_V2.setConfiguration(
+      dialogflowConfig.client_email,
+      dialogflowConfig.private_key,
+      Dialogflow_V2.LANG_ENGLISH_US,
+      dialogflowConfig.project_id
+    );
+  }
+
+  handleGoogleResponse(result) {
+    let text = result.queryResult.fulfillmentMessages[0].text.text[0];
+    this.sendBotResponse(text);
   }
 
   onSend(messages = []) {
     this.setState(previousState => ({
-      messages: GiftedChat.append(previousState.messages, messages),
+      messages: GiftedChat.append(previousState.messages, messages)
     }));
+
+    let message = messages[0].text;
+    Dialogflow_V2.requestQuery(
+      message,
+      result => this.handleGoogleResponse(result),
+      error => console.log(error)
+    );
   }
-  componentDidUpdate() {
-    Dialogflow_V2.setConfiguration(
-      {
-        "type": "service_account",
-        "project_id": "newschat-f0da3",
-        "private_key_id": "8f4b035c2c6c29c7ab693b1b4a1fd88713ba2e02",
-        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDdbjXt+WdOnXJm\njzAPp5jBgTRtZe9CNp5/2ioWJ5xzyMF3VJ8Z4X3WDfFIfYZCj29pGiC/gPQAZ69D\nsBtUITKmiwPwP8Gi2Ce91cWftOSWklp77zKj8nDKyEqouzwrf/HasdZAZg3jNrBx\nqJqNyOgWGfW5s2YnLGKtKkX0e/IrcxqQOygz3csH3P3OpHqawqYfP9esb9YDyHw4\nUgKB6g0k9Gea2QeAsNe8W6jKe6avYMiCt1w5BUdzrFRRH62VjARyVULCzFtWBodc\nNuemZu32RXjgnqL8CajSJJZIp1oOvOjq1fNloZoGRuVqJldI6BdSUWSrH7grjPVV\nvu7JNMPNAgMBAAECggEAApM8YP2fCXIu+T6zY43ilWbaxLuZlOQMuNZBL2EbjS8p\nrNPDm+nxlt0yT/0BuYwbKBZBM5Oc2CpchprUM94aTYcyrwcWyegzTMkL030nkWa3\nUwH89/IjBKtf+ZFf3u5N0j9yNe0vPn64KOzkiz8/D7ln5avOmtMF0hdlQ+AdzgqI\nRQgvBO+MjY4ZkTfnzGkxftyL5sYjR/VqHKIgoGdd/Mhuk5lzeyzhTo1K6orqxgSM\ns8WA47yidHekRaiH2alIZaWafQFxwY3EURXRTqm3qpheMlEIa+Zde+hNYZ5r/WRK\npBN2g7wvmjv9nfyBU/wOdfupIulxWg8cuaTcw3BOAQKBgQD58AvdO+K7IW8aCf3W\nvo+xF+Kz63EQ098wjGog10Qnyeh0o78v02ToBIhMFRgZNQXzEXk2YbXnREjinaZj\nNXrZNx/bhb1ws9D5IQAueJnTQlGsD9cNqA2rjgwjNWDMYhyR+9nwJQd8BdscJFE2\neUdKqznBnO8uNe8mZn7S2gO8zQKBgQDizScojm8dMEXEMzjbLMITS8IYuuFOTr86\n+SKEnbf1UNRLoNNHmJMJt0nUAL0vARqbHVCC6Y053Csotp51eNJlFUDj0KVqt921\nlFiSZAnJ7f4rpBX9BuodTrERalX3tWB0Xi7svM/bd6D2O6ivkL6ZeRdfdlfcXM0S\nldHbreUjAQKBgA+GiMYiXTAwoH0ml70KM1TV3gA9JNXdw2R5pHILzTEHisgMD+lB\noWgEOA851Dhkocz6jeQB59HI3RountJU1R+/G9yAD0d79AQ1t5Qra6rC2N3uUfaI\nNOkuNrTNMQkhIxMyaNRRCIj8uCKkYuMWX4ZU4o6TWAwvz348Ka6hWlAVAoGAYcOB\nL7BGIVTfnfVeesvOVLcBUduspy62q9sckAHUfEB9YqROrrwMFdIWVvaCljJpn5eB\nTw0RvXE0gXZG0+hi+QOyVwFnbJsZTN5HV8MbnRmjyHgQqmNCo6N6HEG0EPT0QPUg\nURgKmVX14m5RBmzkXA+gjZvbv7eZBnviXEmXTgECgYEA3GzalnYVJM5LDiGxYOJP\nuwt0LDjk2c2AiveGdiE8in12Q02G2Gt3pc9Tj883tjKAUm6MYbq3M44ttQIfaz/g\npv/LfVnqx/HmgoT17scCxENMxP9IzYOTil8oQAbuV6janlp98bTHcIWEIJ4NjZJz\nHVRXv0Bz15iCVhU/hwSzEEY=\n-----END PRIVATE KEY-----\n",
-        "client_email": "dialogflow-byijam@newschat-f0da3.iam.gserviceaccount.com",
-        "client_id": "116446060547353387203",
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/dialogflow-byijam%40newschat-f0da3.iam.gserviceaccount.com"
-      }
-      
-);
+
+  sendBotResponse(text) {
+    let msg = {
+      _id: this.state.messages.length + 1,
+      text,
+      createdAt: new Date(),
+      user: BOT_USER
+    };
+
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, [msg])
+    }));
   }
 
   render() {
     return (
-      <View style={{flex: 1}}>
+      <View style={{ flex: 1, backgroundColor: '#fff' }}>
         <GiftedChat
           messages={this.state.messages}
           onSend={messages => this.onSend(messages)}
@@ -58,3 +77,5 @@ export default class App extends React.Component {
     );
   }
 }
+
+export default App;  
