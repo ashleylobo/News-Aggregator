b1219e9467aba37b65d39e5d0180d1636f9af5f9
Suyash <suyash.sreekumar@gmail.com>
2019-03-30 18:15:16 +0530

    Node with db linked

--- news-backend/model/dbconnection.js
+++ news-backend/model/dbconnection.js
@@ -1,6 +1,6 @@
 var mongoose=require("mongoose")
 
-mongoose.connect("mongodb://localhost/sample",{ useNewUrlParser: true } )       //run mongod -dbpath directory for local database
+mongoose.connect("mongodb://bug busters:bugbusters10@ds119606.mlab.com:19606/news-aggregator",{ useNewUrlParser: true } )       //run mongod -dbpath directory for local database
 mongoose.connection
 .once("open",()=>{
 console.log("Database is connected")
